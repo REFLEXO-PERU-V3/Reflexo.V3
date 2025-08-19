4fd21348f4fd828eaa6d25d8beead4f75bcece0d
RONAL <ronalasencio13@gmail.com>
2025-08-19 10:49:57 -0500

    Primer inicio

---
+++ Reflexo-V3/src/components/Button/CustomButtom.jsx
@@ -0,0 +1,26 @@
+import { Button, ConfigProvider } from 'antd';
+
+const CustomButton = ({text, onClick}) => {
+    return(
+        <ConfigProvider
+            theme={{
+                components: {
+                    Button: {
+                        colorPrimary: '#00AA55',
+                        algorithm: true,
+                        },
+                },
+            }}
+        >
+            <Button 
+                type="primary"
+                onClick={onClick}
+                size="large"
+            >
+                {text}
+            </Button>
+        </ConfigProvider>
+    );
+};
+
+export default CustomButton;
